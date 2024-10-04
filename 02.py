2fa72be68905c9b11eb0e28dd7ff786fcb8d0b8c
ReioV69 <reio@hkhk.edu.ee>
2024-10-04 13:36:30 +0300

    Add files via upload

--- 02.py
+++ 02.py
@@ -4,6 +4,37 @@ from math import *
 # 30.09.24
 
 
+# kolmnurga ümbermõõt
+a = int(input("Lisa arv: "))
+b = int(input("Lisa arv: "))
+c = int(input("Lisa arv: "))
+p = a + b + c
+print("kolmnurga ümbermõõt on", p,"ühikut")
+
+#toote hind
+tootehind = 36.75
+ale = 0.4
+soodushind = tootehind - (tootehind * ale)
+kogus = 3
+kokku = soodushind * kogus
+
+print(kokku,"€")
+
+
+
+# 6. Rulluisutajad
+kiirus = 29.9
+aeg = 24/60
+dist = kiirus * aeg
+print("Rulluisutaja jõuab" , dist, "km kaugusele")
+
+
+#pitsa
+hind = 14.19
+hind1 = hind / 3
+print("Igaüks maksab" , hind1, "€")
+
+
 
 #Hüpotenuus
 a,b = 16,9
@@ -24,7 +55,7 @@ m = aeg % 60
 print(h,":",m)
 
 #Arvusüsteemid
-arv = int(input("Lisa arv: "))
+arv = int(input("Lisa 10nd arv: "))
 kahend = bin(arv)
 kuust = hex(arv)
 print("Sinu teisendused on: ", kahend, "ja", kuust)
