8c2128c2f52d5d5ca7d160eb61ce8bc8725176f4
ReioV69 <reio@hkhk.edu.ee>
2024-10-16 09:19:36 +0300

    Update yl4.py

--- yl4.py
+++ yl4.py
@@ -1,1 +1,15 @@
-print("tere maailm")
+# 16.10.2024
+# Ülesanded 4
+
+#Raamatute allahindulus
+ale = 0.3
+hind = 12.5
+kogus = int(input("Lisa raamatute kogus: ")) 
+summa = (hind-(hind*ale)) * kogus
+print(f"{kogus} raamatu hind soodustusega on {summa:0.2f}€.")
+
+#Aia pikkus
+a = int(input("Lisa 1 külg: "))
+b = int(input("Lisa 2 külg: "))
+p = 2*(a+b)
+print(f"Aia kogupikkus on {p} meetrit.")
