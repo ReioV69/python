d259952834de1d7e581ecd6696bebd2b21b7a775
ReioV69 <reio@hkhk.edu.ee>
2024-11-19 14:25:50 +0200

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,53 @@
+# Reio Viikmaa
+# 19.11.24
+
+# kytus = lambda kytusekulu, kaugus: (kytusekulu / 100) * kaugus
+# print(kytus(5, 150))
+
+
+# def temp(t, yhik):
+#     """
+#     Teisenda temperatuuri Celsiusest Fahrenheitiks ja vastupidi.
+ 
+#     Parameetrid:
+#     t (int): Temperatuur.
+#     yhik (String): "C" või "F".
+ 
+#     Tagastab:
+#     int: temperatuuri.
+ 
+#     Näide:
+#     >>> temp(0, "C")
+#     32
+#     >>> temp(0, "F")
+#     -17.78
+#     """
+#     if yhik=="C":
+#         v = t * 5/9+32
+#     else:
+#         v = (t-32) * 5/9
+ 
+#     return round(v,2)
+
+# print(temp.__doc__)
+# print(temp(0,"F"))
+
+konto = 10
+
+def konto_haldur(algne_saldo, toiming, summa):
+    global konto
+    if toiming == "deposiit":
+        v = algne_saldo+summa
+        konto = v
+    else:
+        v = summa - algne_saldo
+        konto = v
+    return v
+
+
+
+
+
+print(konto_haldur(20, "deposiit", konto))
+print(konto_haldur(10, "deposiit", konto))
+print(konto_haldur(20, "väljavõte", konto))
