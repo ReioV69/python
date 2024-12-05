52d5749347d26bf4c1f8689320f0ba96ea7e431f
ReioV69 <reio@hkhk.edu.ee>
2024-12-05 11:37:20 +0200

    Create python-töö3.py

---
+++ python-töö3.py
@@ -0,0 +1,21 @@
+
+fail = open("rebased.txt", encoding="UTF-8")
+
+vastuvõetud = []
+
+for rida in fail:
+    vastuvõetud.append(int(rida))
+print(vastuvõetud)
+fail.close()
+
+
+a = int(input("Palun sisestage, millise aasta andmeid vajate?"))
+
+print(vastuvõetud[int(a[3])-1])
+
+
+fail = open("konto.txt", encoding="UTF-8")
+
+for rida in fail:
+    if int(rida) > 0:
+        print(rida, end="")
