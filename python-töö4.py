37abb0f78b74b21a25363578de2c3acd86938f46
ReioV69 <reio@hkhk.edu.ee>
2024-12-10 11:43:19 +0200

    Create python-töö4.py

---
+++ python-töö4.py
@@ -0,0 +1,23 @@
+def mahlapakkide_arv(ountekogus):
+    mahlapakkidearv = int(round(ountekogus * 0.4/3,0))
+    return mahlapakkidearv
+kg = int(input("Õunte kogus: "))
+print(mahlapakkide_arv(kg))
+
+
+
+# mitu = int(input("Mitu korda: "))
+# lause = input("Sinu reklaamlause: ")
+
+# for i in range(mitu):
+#     print(lause.upper)
+
+# def banner(l):
+#     print(l.upper())
+
+# for i in range(mitu):
+#     banner(lause)
+
+#4.3
+def eelarve(kylalistearv):
+    
