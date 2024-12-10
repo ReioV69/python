8563aebf8e658dd9d4cdd275c1bf1932d026c4a2
ReioV69 <reio@hkhk.edu.ee>
2024-12-10 11:43:12 +0200

    Update python-töö3.py

--- python-töö3.py
+++ python-töö3.py
@@ -1,21 +1,42 @@
 
-fail = open("rebased.txt", encoding="UTF-8")
+# fail = open("rebased.txt", encoding="UTF-8")
 
-vastuvõetud = []
+# vastuvõetud = []
 
-for rida in fail:
-    vastuvõetud.append(int(rida))
-print(vastuvõetud)
-fail.close()
-
-
-a = int(input("Palun sisestage, millise aasta andmeid vajate? "))
-
-print(vastuvõetud[int(a[3])-1])
+# for rida in fail:
+#     vastuvõetud.append(int(rida))
+# print(vastuvõetud)
+# fail.close()
 
 
-fail = open("konto.txt", encoding="UTF-8")
+# a = int(input("Palun sisestage, millise aasta andmeid vajate? "))
 
-for rida in fail:
-    if int(rida) > 0:
-        print(rida, end="")
+# print(vastuvõetud[int(a[3])-1])
+
+
+# fail = open("konto.txt", encoding="UTF-8")
+
+# for rida in fail:
+#     if int(rida) > 0:
+#         print(rida, end="")
+
+
+# valik = input("Sisesta faili nimi:")
+# fail = open(valik)
+# read = fail.readlines()
+# mitmes = 1
+# for i in read:
+#     print(mitmes, i, end="")
+#     mitmes +=1
+
+# lugu = int(input(" Siseta loonumber: "))
+# fail = open(valik)
+# read = fail.readlines()
+
+# print("mängi lugu",read[lugu-1])
+
+from datetime import *
+
+fail = open("nimekiri.txt")
+read = fail.readlines()
+print(read[datetime.now().day-1])
