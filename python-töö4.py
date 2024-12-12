9d4d629cb37d5d234012ec521403db3ee984c2bb
ReioV69 <reio@hkhk.edu.ee>
2024-12-12 09:58:07 +0200

    Update python-töö4.py

--- python-töö4.py
+++ python-töö4.py
@@ -1,11 +1,12 @@
-def mahlapakkide_arv(ountekogus):
-    mahlapakkidearv = int(round(ountekogus * 0.4/3,0))
-    return mahlapakkidearv
-kg = int(input("Õunte kogus: "))
-print(mahlapakkide_arv(kg))
+#4.2
+# def mahlapakkide_arv(ountekogus):
+#     mahlapakkidearv = int(round(ountekogus * 0.4/3,0))
+#     return mahlapakkidearv
+# kg = int(input("Õunte kogus: "))
+# print(mahlapakkide_arv(kg))
 
 
-
+#4.1
 # mitu = int(input("Mitu korda: "))
 # lause = input("Sinu reklaamlause: ")
 
@@ -19,5 +20,35 @@ print(mahlapakkide_arv(kg))
 #     banner(lause)
 
 #4.3
-def eelarve(kylalistearv):
-    
+
+# def eelarve(kylalistearv):
+#     eelarve = kylalistearv * 10 + 55
+#     return eelarve
+
+# max_inimesed  = int(input("Mitu inimest on kutsutud: ?"))
+
+# min_inimesed = int(input("Mitu inimest tuleb: ?"))
+# print(f"Maksimaalne eelarve: {eelarve(max_inimesed)}")
+# print(f"Minimaalne eelarve: {eelarve(min_inimesed)}")
+
+
+
+
+#4.4
+
+# def tervitus(tere):
+#     for i in range(tere):
+#         print("Võõrustaja: " "Tere!")
+#         print(f"Täna {i+1}, kord tervitada, mõtiskleb võõrustaja.")
+#         print("Külaline: " "Tere, suur tänu kutse eest!")
+        
+
+# tervitus(5)
+
+
+#4.5
+valik = int(input("Sisesta faili nimi: "))
+def pronksikarva_summa(pronks):
+    fail = open("mündid.txt", encoding="UTF-8")
+    fail.readlines()
+
